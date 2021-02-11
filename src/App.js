6b00426e6fa2725701f36a72db4689bda7e85378
iqbal1361 <iqbalhossmd814@gmail.com>
2021-02-12 01:03:07 +0600

    Unsplash Clone Using API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [search, searchArticle]=useState("");
+  const [results, setResult]=useState([]);
+
+  //fvJ7-FMlT_zoHm_ABdfFwxscYL5z74zFaCPuiSg8NJQ
+  //https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+  
+   const images=()=>{
+
+    axios.get(`https://api.unsplash.com/search/photos?client_id=fvJ7-FMlT_zoHm_ABdfFwxscYL5z74zFaCPuiSg8NJQ&query=${search}`).then(response=>{
+      console.log(response.data.results)
+      setResult(response.data.results)
+    })
+
+   }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="content">
+       <span>Search: </span>
+       <input type="text"
+        value={search}
+        id="inputField"
+        onChange={ (e)=>searchArticle(e.target.value) }
+       />
+       <button onClick={()=>images()}>Search</button>
+      </div>
+      
+      <div className="gallery">
+         
+         {
+
+          results.map((result)=>{
+            return <img className="item" src={ result.urls.regular }/>
+          })
+
+         }
+
+      </div>
+
     </div>
   );
 }
